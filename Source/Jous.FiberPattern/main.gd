99a065d8d8f3838c7aaa41bab83b6d58cf0a08b1
Alexander Griffin <alexander_goudjeb@hotmail.com>
2024-11-05 06:31:03 +0100

    FiberPattern 1.0.1

--- Source/Jous.FiberPattern/main.gd
+++ Source/Jous.FiberPattern/main.gd
@@ -13,10 +13,12 @@ func _ready():
 
 	# Add our pattern as an option.
 	Lure.add_content(ID, "Fiber","mod://pattern_fiber.tres")
+	Lure.add_content(ID, "Fiber_Sack","mod://pattern_fiber_sack.tres")
 
 	# Add patterns to species
 	for Specie in Lure.modded_species:
 		Lure.assign_pattern_texture(ID, ID + ".Fiber", Specie, "mod://body_pattern_fiber.png")
+		Lure.assign_pattern_texture(ID, ID + ".Fiber_Sack", Specie, "mod://body_pattern_fiber_sack.png")
 		
 	# Refresh patterns to fix other species.
 	Lure.Loader._refresh_patterns()
